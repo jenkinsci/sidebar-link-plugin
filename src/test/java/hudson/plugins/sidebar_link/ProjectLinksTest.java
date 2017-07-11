c589a07ecf2489c43da5c2617a14080e660a86d3
Alex Johnson <alexjo5000@gmail.com>
2017-07-11 11:53:12 -0400

    Compatability with workflow

--- src/test/java/hudson/plugins/sidebar_link/ProjectLinksTest.java
+++ src/test/java/hudson/plugins/sidebar_link/ProjectLinksTest.java
@@ -10,10 +10,17 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.model.AbstractProject;
 import hudson.model.FreeStyleProject;
 import java.io.IOException;
+import java.util.Collections;
+
+import hudson.model.Job;
 import junit.framework.Assert;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -36,5 +43,15 @@ public class ProjectLinksTest {
         rule.submit(form);
         Assert.assertNotNull("Method geJobActions should not return null.", links.getJobActions(project));
     }
-    
+
+    @Test
+    public void testLinkActionIsAddedWithJobProps () throws Exception{
+        Job p1 = rule.jenkins.createProject(FreeStyleProject.class, "burt");
+        p1.addProperty(new ProjectLinks(Collections.singletonList(new LinkAction("http://google.com","stop","edit-delete.png"))));
+        assertEquals(1, p1.getActions(LinkAction.class).size());
+
+        Job p2 = rule.jenkins.createProject(WorkflowJob.class, "ernie");
+        p2.addProperty(new ProjectLinks(Collections.singletonList(new LinkAction("http://giggle.com","stop","edit-delete.png"))));
+        assertEquals(1, p2.getActions(LinkAction.class).size());
+    }
 }
